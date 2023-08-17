0f221ad79d3edaec772963ccb35652fe169bded7
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-08-17 18:08:32 +0300

    working on modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { Modal } from "@/components/ui/modal"
+
 export default function Home() {
   return (
     <section className="container">
       <div className="grid items-center gap-6 py-6">
-        Lorem ipsum dolor sit amet.
+        <Modal title="test" description="test" isOpen onClose={() => {}}>
+          Children
+        </Modal>
       </div>
     </section>
   )
