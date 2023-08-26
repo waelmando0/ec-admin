b1944e06e3d1fe7e43141a730f55ab8dec99b406
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-08-26 19:46:08 +0300

    working on SettingsForm and api routes (DELETE, PATCH)

--- app/(dashboard)/[storeId]/(routes)/settings/components/settingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settingsForm.tsx
@@ -1,18 +1,109 @@
 "use client"
 
+import { useState } from "react"
+import { useParams, useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Store } from "@prisma/client"
+import axios from "axios"
+import { Trash } from "lucide-react"
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
+import * as z from "zod"
 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Heading } from "@/components/ui/heading"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
 
 interface SettingsFormProps {
   initialData: Store
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams()
+  const router = useRouter()
+
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  })
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setLoading(true)
+
+      await axios.patch(`/api/stores/${params.storeId}`, data)
+      router.refresh()
+
+      toast.success("Store Updated.")
+    } catch (error) {
+      toast.error("Something went wrong.")
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div className="flex items-center justify-between">
-      <Heading title="Settings" description="Manage store preference" />
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preference" />
+        <Button
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+          disabled={loading}
+        >
+          <Trash className="w-4 h-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 w-full"
+        >
+          <div className="grid grid-cols-2 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+    </>
   )
 }
 
--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -9,7 +9,7 @@ export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
       <h1 className="scroll-m-20 text-3xl font-bold tracking-tight ">
         {title}
       </h1>
-      <p>{description}</p>
+      <p className="text-sm text-muted-foreground">{description}</p>
     </div>
   )
 }
