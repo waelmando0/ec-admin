85a52ce4fb821da64f7be520e5141dd18d575d98
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-08-27 20:38:35 +0300

    start working on billboard route

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,11 @@
+import BillboardClient from "./components/client"
+
 const BillboardsPage = () => {
-  return <div className="py-4">Billboards</div>
+  return (
+    <div className="py-4">
+      <BillboardClient />
+    </div>
+  )
 }
 
 export default BillboardsPage
