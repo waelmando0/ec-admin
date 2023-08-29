875e4f399b5c896575c574e31e4d201a3d5ffe46
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-08-29 17:36:34 +0300

    working on Billboard-from

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs"
+
+import prismadb from "@/lib/prismadb"
+
+import BillboardForm from "./components/billboardForm"
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string }
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  })
+
+  return (
+    <div className="py-4">
+      <div className="flex-1 space-y-4">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  )
+}
+
+export default BillboardPage
