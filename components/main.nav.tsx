22b107d0033654ce1376107bf1c8581cd65e1dbc
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-09-11 15:56:12 +0300

    finish order route

--- components/main.nav.tsx
+++ components/main.nav.tsx
@@ -45,6 +45,11 @@ export function MainNav({
       active: pathname === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: "orders",
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "settings",
       active: pathname === `/${params.storeId}/settings`,
